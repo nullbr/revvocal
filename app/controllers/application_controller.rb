d2b0d60aff303eede1a7acbd1c85fd05d1aa47ee
Bruno Leite <bmarianoleite3@gmail.com>
2023-08-20 15:58:32 -0300

    adding sessions with devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protect_from_forgery except: :create
+
+  def after_sign_in_path_for(_resource)
+    root_path
+  end
+
+  def log_in_user
+    return if user_signed_in?
+
+    redirect_to new_user_session_path, status: :see_other
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = %i[username email password password_confirmation remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: %i[login password]
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,31 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: { case_sensitive: true }
+  validate :validate_username
+
+  attr_writer :login
+
+  def login
+    @login || username || email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(['lower(username) = :value OR lower(email) = :value',
+                                    { value: login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+
+  private
+
+  def validate_username
+    return unless User.where(email: username).exists?
+
+    errors.add(:username, :invalid)
+  end
+end
